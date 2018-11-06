360fb2d4234e1c9e265838c6a5d3f65cceb630dd
Anas <anas_siddiqui@live.com>
2018-11-07 00:57:47 +0100

    first-commit

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -85,6 +85,10 @@ var a = new Date(timestamp * 1000);
   return time;
     
 }
+$scope.sort = function(keyname){
+		$scope.sortKey = keyname;   //set the sortKey to the param passed
+		$scope.reverse = !$scope.reverse; //if true make it false and vice versa
+	}
 
 
 
